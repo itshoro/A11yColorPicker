8ec45dc610e58f95b1b5b1fd4ac6de9c06dd370a
Tim Nelke <tim.nelke@outlook.com>
2021-01-24 20:52:36 +0100

    Implemented Equality operations to HSLColor - `HSLColor` can now be compared to another `HSLColor` - `HSLColor` can now be compared to a `RGBColor` (wip, not implemented yet) - Added `ToString()` method

--- A11yColorPicker.Colors/HSLColor.cs
+++ A11yColorPicker.Colors/HSLColor.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace A11yColorPicker.Colors
 {
-    public class HSLColor : IConvertibleColor, IEquatable<RGBColor>
+    public class HSLColor : IConvertibleColor, IEquatable<HSLColor>, IEquatable<RGBColor>
     {
         public short H { get; }
         public float S { get; }
@@ -28,9 +28,57 @@ namespace A11yColorPicker.Colors
             throw new NotImplementedException();
         }
 
+        public override string ToString()
+        {
+            return $"hsl({H}°, {S * 100}%, {L * 100}%)";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, null))
+                return false;
+            if (ReferenceEquals(obj, this))
+                return true;
+
+            return obj switch
+            {
+                HSLColor hsl => Equals(hsl),
+                RGBColor rgb => Equals(rgb),
+                _ => false
+            };
+        }
+
         public bool Equals([AllowNull] RGBColor other)
         {
+            return this == other;
+        }
+
+        public bool Equals([AllowNull] HSLColor other)
+        {
+            return this == other;
+        }
+
+        public static bool operator ==(HSLColor hslColor1, HSLColor hslColor2)
+        {
+            return hslColor1.H == hslColor2.H && hslColor1.S == hslColor2.S && hslColor1.L == hslColor2.L;
+        }
+        public static bool operator !=(HSLColor hslColor1, HSLColor hslColor2)
+        {
+            return !(hslColor1 == hslColor2);
+        }
+
+        public static bool operator ==(HSLColor hslColor1, RGBColor rgbColor)
+        {
             throw new NotImplementedException();
+        }
+        public static bool operator !=(HSLColor hslColor1, RGBColor rgbColor)
+        {
+            return !(hslColor1 == rgbColor);
+        }
+
+        public override int GetHashCode()
+        {
+            return H * 100 * 100 + (int)(S * 100) * 100 + (int)(L * 100);
         }
     }
 }
